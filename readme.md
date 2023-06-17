304f5d6ac2695d2a1c73675fa7f0ebc8a6e3d1d1
Alexander Menezes <alexmatheus21@dr.com>
2023-06-17 10:40:20 -0300

    stopwatch

---
+++ readme.md
@@ -0,0 +1,5 @@
+Script created by Wizard Laboratory. 
+https://github.com/wizard-laboratory
+
+Minute function updated by Mac Menezes
+https://github.com/alexmatheus21/Mac-Menezes
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.onload = function () {
   
   var seconds = 00; 
+  var minutes = 00; 
   var tens = 00; 
   var appendTens = document.getElementById("tens")
   var appendSeconds = document.getElementById("seconds")
+  var appendMinutes = document.getElementById("minutes")
   var buttonStart = document.getElementById('button-start');
   var buttonStop = document.getElementById('button-stop');
   var buttonReset = document.getElementById('button-reset');
@@ -24,8 +26,10 @@ window.onload = function () {
      clearInterval(Interval);
     tens = "00";
   	seconds = "00";
+    minutes = "00";
     appendTens.innerHTML = tens;
   	appendSeconds.innerHTML = seconds;
+    appendMinutes.innerHTML = minutes;
   }
   
    
@@ -52,6 +56,19 @@ window.onload = function () {
     
     if (seconds > 9){
       appendSeconds.innerHTML = seconds;
+    }
+
+    if (seconds > 60) {
+      console.log("minutes");
+      minutes++;
+      appendMinutes.innerHTML = "0" + minutes;
+      seconds = 0;
+      appendTens.innerHTML = "0" + 0;
+      appendSeconds.innerHTML = "0" + 0;
+    }
+
+    if (minutes > 9){
+      appendMinutes.innerHTML = minutes;
     }
   
   }
